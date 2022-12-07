1f3522ae20989f3992e65efc1dadcfec6409a7b9
Sylvio Sell <info@maitag.de>
2022-12-07 17:08:45 +0100

    fixing macro to load testdata

--- samples/simple/src/AssetMacro.hx
+++ samples/simple/src/AssetMacro.hx
@@ -23,12 +23,17 @@ class AssetMacro {
 		if (!FileSystem.exists(path)) {
 			var data:String;
 			try {
+				// hack around ssl-certification (https://github.com/HaxeFoundation/haxe/issues/9481#issuecomment-633579121)
+				var req = new sys.Http(url);
+				req.setHeader("Agent-Orange", ""); // or any user-agent ;)
+				req.request(false);
+				
+				// loading data
 				data = haxe.Http.requestUrl(url);
-				File.saveContent(path, data);
 			} 
-			catch (e) throw('can\'t load data from $url' + e);			
+			catch (e) throw('\n\nCan\'t load testdata from $url \n(' + e + ')\nPlease load it manually and save into $assetPath!\n');
 		}
-		
+
 		return macro $v{Path.normalize(assetPath)};
 	}
 }
