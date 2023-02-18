0a59c0f833a1382efffd60bb55f89d456a172afb
Sylvio Sell <info@maitag.de>
2023-02-18 22:23:52 +0100

    autonome shift-metakey handling

--- src/peote/ui/interactive/input2action/InputTextPage.hx
+++ src/peote/ui/interactive/input2action/InputTextPage.hx
@@ -21,19 +21,21 @@ class InputTextPage
 		{ action: "cutToClipboard"    , keyboard: [ KeyCode.CUT,   [KeyCode.LEFT_CTRL, KeyCode.X], [KeyCode.RIGHT_CTRL, KeyCode.X] ] },
 		{ action: "pasteFromClipboard", keyboard: [ KeyCode.PASTE, [KeyCode.LEFT_CTRL, KeyCode.V], [KeyCode.RIGHT_CTRL, KeyCode.V] ] },
 		
+		{ action: "selectModifier", keyboard: [KeyCode.LEFT_SHIFT, KeyCode.RIGHT_SHIFT] },
+		
 		{ action: "cursorLeft" , keyboard: KeyCode.LEFT , single:true },
 		{ action: "cursorRight", keyboard: KeyCode.RIGHT, single:true },		
 		{ action: "cursorLeftWord" , keyboard: [ [KeyCode.LEFT_CTRL, KeyCode.LEFT ], [KeyCode.RIGHT_CTRL, KeyCode.LEFT ] ], single:true},
 		{ action: "cursorRightWord", keyboard: [ [KeyCode.LEFT_CTRL, KeyCode.RIGHT], [KeyCode.RIGHT_CTRL, KeyCode.RIGHT] ], single:true},
 		
-		{ action: "selectLeft" , keyboard: [ [KeyCode.LEFT_SHIFT, KeyCode.LEFT ], [KeyCode.RIGHT_SHIFT, KeyCode.LEFT ] ] },
-		{ action: "selectRight", keyboard: [ [KeyCode.LEFT_SHIFT, KeyCode.RIGHT ], [KeyCode.RIGHT_SHIFT, KeyCode.RIGHT ] ] },
+		//{ action: "selectLeft" , keyboard: [ [KeyCode.LEFT_SHIFT, KeyCode.LEFT ], [KeyCode.RIGHT_SHIFT, KeyCode.LEFT ] ] },
+		//{ action: "selectRight", keyboard: [ [KeyCode.LEFT_SHIFT, KeyCode.RIGHT ], [KeyCode.RIGHT_SHIFT, KeyCode.RIGHT ] ] },
 		
 		{ action: "cursorUp"   , keyboard: KeyCode.UP  , single:true },
 		{ action: "cursorDown" , keyboard: KeyCode.DOWN, single:true },
 		
-		{ action: "selectUp"   , keyboard: [ [KeyCode.LEFT_SHIFT, KeyCode.UP   ], [KeyCode.RIGHT_SHIFT, KeyCode.UP   ] ] },
-		{ action: "selectDown" , keyboard: [ [KeyCode.LEFT_SHIFT, KeyCode.DOWN ], [KeyCode.RIGHT_SHIFT, KeyCode.DOWN ] ] },
+		//{ action: "selectUp"   , keyboard: [ [KeyCode.LEFT_SHIFT, KeyCode.UP   ], [KeyCode.RIGHT_SHIFT, KeyCode.UP   ] ] },
+		//{ action: "selectDown" , keyboard: [ [KeyCode.LEFT_SHIFT, KeyCode.DOWN ], [KeyCode.RIGHT_SHIFT, KeyCode.DOWN ] ] },
 		
 		{ action: "enter"      , keyboard: [KeyCode.RETURN, KeyCode.RETURN2, KeyCode.NUMPAD_ENTER] },
 		
@@ -54,19 +56,21 @@ class InputTextPage
 		"cutToClipboard"       => { action:cutToClipboard  },
 		"pasteFromClipboard"   => { action:pasteFromClipboard, repeatKeyboardDefault:true },
 		
+		"selectModifier"      => { action:selectModifier, up:true },
+		
 		"cursorLeft"      => { action:cursorLeft     , repeatKeyboardDefault:true },
 		"cursorRight"     => { action:cursorRight    , repeatKeyboardDefault:true },
 		"cursorLeftWord"  => { action:cursorLeftWord , repeatKeyboardDefault:true },
 		"cursorRightWord" => { action:cursorRightWord, repeatKeyboardDefault:true },		
 		
-		"selectLeft"      => { action:selectLeft     , repeatKeyboardDefault:true },
-		"selectRight"     => { action:selectRight    , repeatKeyboardDefault:true },
+		//"selectLeft"      => { action:selectLeft     , repeatKeyboardDefault:true },
+		//"selectRight"     => { action:selectRight    , repeatKeyboardDefault:true },
 
 		"cursorUp"    => { action:cursorUp   , repeatKeyboardDefault:true },
 		"cursorDown"  => { action:cursorDown , repeatKeyboardDefault:true },
 		
-		"selectUp"    => { action:selectUp   , repeatKeyboardDefault:true },
-		"selectDown"  => { action:selectDown , repeatKeyboardDefault:true },
+		//"selectUp"    => { action:selectUp   , repeatKeyboardDefault:true },
+		//"selectDown"  => { action:selectDown , repeatKeyboardDefault:true },
 		
 		"enter"       => { action:enter      , repeatKeyboardDefault:true },
 	];
@@ -87,21 +91,24 @@ class InputTextPage
 	
 	static inline function copyToClipboard   (_,_) focusElement.copyToClipboard();
 	static inline function cutToClipboard    (_,_) focusElement.cutToClipboard();
-	static inline function pasteFromClipboard(_,_) focusElement.pasteFromClipboard();
+	static inline function pasteFromClipboard(_, _) focusElement.pasteFromClipboard();
 	
-	static inline function cursorLeft     (_,_) focusElement.cursorLeft();
-	static inline function cursorRight    (_,_) focusElement.cursorRight();
+	static var addSelection:Bool = false;
+	static inline function selectModifier (isDown:Bool, _) addSelection = isDown;
+	
+	static inline function cursorLeft     (_,_) focusElement.cursorLeft(addSelection);
+	static inline function cursorRight    (_,_) focusElement.cursorRight(addSelection);
 	static inline function cursorLeftWord (_,_) focusElement.cursorLeftWord();
 	static inline function cursorRightWord(_,_) focusElement.cursorRightWord();	
 	
-	static inline function selectLeft (_,_) focusElement.cursorLeft(true);
-	static inline function selectRight(_,_) focusElement.cursorRight(true);
+	//static inline function selectLeft (_,_) focusElement.cursorLeft(true);
+	//static inline function selectRight(_,_) focusElement.cursorRight(true);
 
 	static inline function cursorUp   (_,_) focusElement.cursorUp();
 	static inline function cursorDown (_,_) focusElement.cursorDown();
 	
-	static inline function selectUp  (_,_) focusElement.cursorUp(true);
-	static inline function selectDown(_,_) focusElement.cursorDown(true);
+	//static inline function selectUp  (_,_) focusElement.cursorUp(true);
+	//static inline function selectDown(_,_) focusElement.cursorDown(true);
 
 	static inline function enter(_,_) focusElement.enter();
 
